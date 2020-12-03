fd7fbd275a8f83c64c18fbab10cb38176f1d8e72
ericmbouwe <ericmbouwe@yahoo.fr>
2020-12-03 18:44:32 +0100

    carousel added

--- js/tech.js
+++ js/tech.js
@@ -151,3 +151,34 @@ toolsLink.addEventListener('click', () => {
 lifeCycleLink.addEventListener('click', () => {
   handleTabClick(lifeCycleLink, lifecycleContent, techsBlock);
 });
+
+
+
+// $('.loop').owlCarousel({
+//   center: true,
+//   items: 2,
+//   loop: true,
+//   margin: 10,
+//   responsive: {
+//     600: {
+//       items: 4
+//     }
+//   }
+// });
+
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 3
+    },
+    1000: {
+      items: 5
+    }
+  }
+})
