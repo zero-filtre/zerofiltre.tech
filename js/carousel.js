9615384d38bd85008f384b556dc686f04b423698
ericmbouwe <ericmbouwe@yahoo.fr>
2021-02-17 11:53:18 +0100

    fixed footer font sizes

--- js/carousel.js
+++ js/carousel.js
@@ -1,20 +1,7 @@
 let hiddenAboutText = document.querySelector(
   '.about-description span:first-of-type'
 );
-let unFoldButton = document.querySelector('.doUnfold span:first-of-type');
-let foldButton = document.querySelector('.doUnfold span:nth-of-type(2)');
-let unfoldChevron = document.querySelector('.doUnfold img');
 
-const folder = document.getElementById('doUnfold');
-
-if (folder) {
-  folder.addEventListener('click', (e) => {
-    hiddenAboutText.classList.toggle('unfold');
-    foldButton.classList.toggle('unfold');
-    unFoldButton.classList.toggle('unfold');
-    unfoldChevron.classList.toggle('up');
-  });
-}
 
 const backToTopBtn = document.getElementById('back-to-top');
 window.onscroll = () => {
@@ -65,11 +52,6 @@ function closeSidebar() {
 openSidebarBtn.addEventListener('click', openSidebar);
 
 dropdown.addEventListener('click', () => {
-  // if (dropdownContent.classList.contains('dropdown-expanded')) {
-  //   dropdownContent.classList.remove('dropdown-expanded');
-  // } else {
-  //   dropdownContent.classList.add('dropdown-expanded');
-  // }
   dropdownContent.classList.toggle('dropdown-expanded');
 });
 
@@ -79,37 +61,3 @@ dropdown.addEventListener('click', () => {
 //     console.log('outside click');
 //   }
 // })
-
-let index = 0;
-let list = document.querySelectorAll('.reviews-carousel .card');
-let count = list.length;
-let stop = false;
-
-var slide = setInterval(() => {
-  if (!stop) {
-    console.log('Start');
-    if (list[index]) {
-      list[index].classList.toggle('invisible');
-      index++;
-      if (index + 1 > count) {
-        index = 0;
-      }
-      list[index].classList.toggle('invisible');
-    }
-  }
-}, 10000);
-
-if (list) {
-  list.forEach((el) => {
-    //stop slide show on hover but restart after 20s
-    el.addEventListener('mouseover', function (e) {
-      console.log('Stop');
-      stop = true;
-
-      setTimeout(() => {
-        console.log('Restart');
-        stop = false;
-      }, 15000);
-    });
-  });
-}
